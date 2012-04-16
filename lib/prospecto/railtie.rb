82637b343a8bab7ff437912c6312c315e2865ee8
Jason Webb <bigjasonwebb@gmail.com>
2012-04-16 13:47:28 -0600

    Add views to the auto load path during init

--- lib/prospecto/railtie.rb
+++ lib/prospecto/railtie.rb
@@ -5,5 +5,11 @@ module Prospecto
     initializer "prospecto.initialize" do |app|
       ActionController::Base.extend(ProspectoController)
     end
+
+    initializer 'prospecto.autoload', :before => :set_autoload_paths do |app|
+      Dir["#{app.root}/app/views/**/"].each do |view_folder|
+        app.config.autoload_paths << view_folder
+      end
+    end
   end
 end
