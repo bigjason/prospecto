18a0240f3863e006b4e9dd62ae7789481e57d1df
Jason Webb <bigjasonwebb@gmail.com>
2012-04-16 16:39:00 -0600

    Automatically modify the search path to include the new structures

--- lib/prospecto/controller.rb
+++ lib/prospecto/controller.rb
@@ -1,8 +1,6 @@
-require 'rails/railtie'
-
 module Prospecto
   module ProspectoController
-    def prospecto
+    def enable_prospecto
       view_path = Rails.root.join("app/views")
       resolver = ActionView::FileSystemResolver.new(view_path, ":prefix/templates/:action{.:locale,}{.:formats,}{.:handlers,}")
       prepend_view_path(resolver)
--- lib/prospecto/railtie.rb
+++ lib/prospecto/railtie.rb
@@ -1,9 +1,11 @@
+require 'rails/railtie'
 require 'prospecto/controller'
 
 module Prospecto
   class Railtie < Rails::Railtie
     initializer "prospecto.initialize" do |app|
       ActionController::Base.extend(ProspectoController)
+      ActionController::Base.class_eval { enable_prospecto }
     end
 
     initializer 'prospecto.autoload', :before => :set_autoload_paths do |app|
