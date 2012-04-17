a046f886d1ebd269ace63452e63c28c8de93beb1
Jason Webb <bigjasonwebb@gmail.com>
2012-04-16 21:53:12 -0600

    Fix tests.

--- lib/prospecto/view.rb
+++ lib/prospecto/view.rb
@@ -3,6 +3,8 @@ module Prospecto
     # lookup_context is required for rspec integration to work.
     attr_reader :lookup_context
 
+    # Convenience method to create a constructor and attr_reader
+    # for the specified attributes.
     def self.accepts(*names)
       attr_accessor *names
       define_method :initialize do |args={}|
--- test/view_test.rb
+++ test/view_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class ViewTest < ActiveSupport::TestCase
-  test "auto generates a constructor and attr_readers" do
-    class TestConstructor < Prospecto::View
-      accepts :name, :age
-    end
+  class TestConstructor < Prospecto::View
+    accepts :name, :age
+  end
+
+  test "auto generates a constructor" do
+    assert TestConstructor.new(:name => "Bob", :age => 44).present?
+  end
+
+  test "auto generates attr_readers" do
     person = TestConstructor.new(:name => "Bob", :age => 44)
     assert_equal person.name, "Bob"
     assert_equal person.age, 44
